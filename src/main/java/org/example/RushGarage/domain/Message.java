c5dc9e4ae0dbe66d4e84a11e894d84ec904b6f97
sankaaa62 <kandakov.sasha@gmail.com>
2020-01-12 19:46:51 +0500

    Добавлена загрузка изображений и раздача статики

--- src/main/java/org/example/RushGarage/domain/Message.java
+++ src/main/java/org/example/RushGarage/domain/Message.java
@@ -15,6 +15,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message() {
     }
 
@@ -23,6 +25,7 @@ public class Message {
         this.text = text;
         this.tag = tag;
     }
+
 
     public String getAuthorName (){
         return author != null ? author.getUsername() : "<none>";
@@ -58,5 +61,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
